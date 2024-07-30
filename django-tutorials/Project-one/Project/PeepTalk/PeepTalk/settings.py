69ef9ca2e53ecb9d8dfbad99beb14231fd59b453
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2024-07-30 22:25:13 +0530

    setup

--- django-tutorials/Project-one/Project/PeepTalk/PeepTalk/settings.py
+++ django-tutorials/Project-one/Project/PeepTalk/PeepTalk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os #setup
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media files (uploads) #setup
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #setup
+
+
+# Static files (CSS, JavaScript, Images)  #setup
+STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
